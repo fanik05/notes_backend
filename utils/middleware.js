ed026e4c992157f477139a37ebb9effa04530228
fanik05 <fatinanik129@gmail.com>
2022-03-31 14:50:17 +0600

    token expire time added

--- utils/middleware.js
+++ utils/middleware.js
@@ -13,6 +13,8 @@ const unknownEndpoint = (request, response) => {
 }
 
 const errorHandler = (error, request, response, next) => {
+  logger.error(error.message)
+
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
@@ -21,9 +23,11 @@ const errorHandler = (error, request, response, next) => {
     return response.status(401).json({
       error: 'invalid token',
     })
+  } else if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({
+      error: 'token expired',
+    })
   }
-
-  logger.error(error.message)
 
   next(error)
 }
