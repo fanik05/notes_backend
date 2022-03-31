1212f0131954741e721c27f4b383a036a5b4f7c7
fanik05 <fatinanik129@gmail.com>
2022-03-31 10:05:17 +0600

    prettified

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   rules: {
     indent: ['error', 2],
-    'linebreak-style': ['error', 'windows'],
+    'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'never'],
     eqeqeq: 'error',
